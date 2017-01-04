1ed79845a734f446a3e4e61e54816acdca8642b9
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-04 17:44:01 +0000

    Initial support for linting

--- Jakefile.js
+++ Jakefile.js
@@ -1,4 +1,11 @@
-desc('Default task')
-task('default', function(){
+desc('Default task');
+task('default', ['lint'], function(){
     console.log('\n\nBuild OK');
+});
+
+desc('Lint Everything');
+task('lint', [], function(){
+    console.log('Lint code goes here');
+    var lint = require('./build/lint/lint_runner.js');
+    lint.validateFile('Jakefile.js', {}, {});
 });
