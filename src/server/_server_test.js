36034600ac09a0a0843a24699cd347a3f3ce294d
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-05 13:21:55 +0000

    Use nodeunit assertions

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,10 +1,8 @@
 (function() {
     'use strict';
 
-    var assert = require('assert');
-
     exports.testNothing = function(test){
-        assert.equal(3, 3, 'number');
+        test.equals(3, 3, 'number');
         test.done();
     };
 
