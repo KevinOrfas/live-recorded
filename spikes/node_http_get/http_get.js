f40bdfd5a07f5b9ccd1a81f70c6f5d7074d7968b
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-05 15:40:19 +0000

    Modified address to be localhost

--- spikes/node_http_get/http_get.js
+++ spikes/node_http_get/http_get.js
@@ -3,7 +3,7 @@
 
     var http = require('http');
 
-    http.get('http://127.0.0.1:8080', function(res) {
+    http.get('http://localhost:8080', function(res) {
         console.log('Got response: ' + res.statusCode);
     }).on('error', function(e) {
         console.log('Got error: ' + e.message);
