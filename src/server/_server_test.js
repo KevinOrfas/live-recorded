69a8b0b988a093bf527e2c1de818c572a605f8b2
Anastasios Orfanidis <aorfanidis@tagworldwide.com>
2017-01-05 15:57:28 +0000

    First server test

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,13 +4,23 @@
     var server = require('./server.js');
     var http = require('http');
 
-    exports.testHttpServer = function(test) {
-        server.start();
-        http.get('http://localhost:8080', function(response){
-
+    exports.tearDown = function(done) {
+        server.stop(function() {
+            done();
         });
+    };
 
-        test.done();
+    //TODO: handle case where stop() is called before start()
+    //TODO: test-drive stop() callback
+
+    exports.testServerRespondsToGetRequests = function(test) {
+        server.start();
+        http.get('http://localhost:8080', function(response) {
+            response.on('data', function(){
+                console.log('response', response);
+            });
+            test.done();
+        });
     };
 
 }());
--- src/server/server.js
+++ src/server/server.js
@@ -1,31 +1,21 @@
+/*jshint esversion: 6 */
 (function() {
     'use strict';
 
+    var http = require('http');
+    var server;
+    var port = 8080;
+
     exports.start = function() {
-        var http = require('http');
-
-        var server = http.createServer();
-
-        server.on('request', function(request, response){
-            console.log('Received request');
-
-            var body = '<html><head><title>Node HTTP Spike</title></head>' +
-                    '<body><p>This is a spike of Node\'s HTTP server.</p></body></html>';
-
-            // The following two approaches are equivalent:
-            // The verbose way...
-            //	response.statusCode = 200;
-            //	response.write(body);
-            //	response.end();
-
-            // The concise way...
-            response.end(body);
+        server = http.createServer();
+        server.on('request', function(request, response) {
+            response.end();
         });
+        server.listen(port);    //TODO: Remove duplication
+    };
 
-
-        server.listen(8080);
-
-        console.log('Server running localhost:8080');
+    exports.stop = function(callback) {
+        server.close(callback);
     };
 
 }());
